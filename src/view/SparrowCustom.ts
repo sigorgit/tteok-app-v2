e3de49fc64d8281ae852706f9cb33b8ac7f20bed
Hanul <hanul@hanul.me>
2022-01-22 16:41:02 +0900

    버그 수정

--- src/view/SparrowCustom.ts
+++ src/view/SparrowCustom.ts
@@ -1,5 +1,6 @@
 import { DomNode, el } from "@hanul/skynode";
-import { View, ViewParams } from "skyrouter";
+import { SkyRouter, View, ViewParams } from "skyrouter";
+import superagent from "superagent";
 import SparrowPreview from "../component/SparrowPreview";
 import InjeolmiContract from "../contracts/InjeolmiContract";
 import ItemStoreContract from "../contracts/ItemStoreContract";
@@ -33,111 +34,141 @@ export default class SparrowCustom implements View {
     private async load(id: number) {
 
         const ment = await SparrowsContract.ments(id);
-        const metadata = await KlubsLoader.loadMetadata(SparrowsContract.address, id);
+        const metadata = (await superagent.get(`https://api.tteok.org/sparrows/${id}`)).body;
 
         let preview: SparrowPreview;
         let input: DomNode<HTMLInputElement>;
 
-        this.container.append(el("main",
-            preview = new SparrowPreview(metadata.attributes, ment),
-            el(".form",
-                el(".change-ment",
-                    el("p", "말풍선을 변경하는데는 100 IJM이 필요합니다."),
-                    input = el("input", {
-                        placeholder: "말풍선 멘트",
-                        value: ment,
-                        keyup: () => {
-                            preview.changeMent(input.domElement.value);
-                        },
-                    }),
-                    el("button", "변경하기", {
-                        click: async () => {
-                            const owner = await Wallet.loadAddress();
-                            if (owner !== undefined) {
-                                if ((await InjeolmiContract.balanceOf(owner)).lte(await SparrowsMentorContract.price())) {
-                                    alert("절미 부족");
-                                } else {
-                                    await SparrowsMentorContract.changeMent(id, input.domElement.value);
-                                    KlubsLoader.refreshMetadata(SparrowsContract.address, id);
-                                    ViewUtil.waitTransactionAndRefresh();
-                                }
-                            }
-                        },
-                    }),
-                ),
-                el(".change-parts",
-                    el("p", "모습을 변경하는데는 100 IJM이 필요합니다."),
-                    ...parts.map((p) => el(".select-wrapper",
-                        el("span", p.name),
-                        el("select",
-                            {
-                                placeholder: p.name,
-                                change: (event, select) => {
-                                    const value = (select.domElement as HTMLSelectElement).value;
-                                    const a = metadata.attributes.find((a: any) => a.trait_type === p.name);
-                                    const traits: any = p.parts.filter((part) => part.name === value);
-                                    for (const trait of traits) {
-                                        if (
-                                            trait.condition === undefined ||
-                                            metadata.attributes.find((d: any) => d.trait_type === trait.condition.trait && trait.condition.values.includes(d.value) === true) !== undefined
-                                        ) {
-                                            if (a === undefined) {
-                                                metadata.attributes.push({ trait_type: p.name, value });
-                                            } else {
-                                                a.value = value;
-                                            }
-                                            preview.changeAttributes(metadata.attributes);
-                                            break;
-                                        }
-                                    }
-                                },
+        this.container.append(
+            el("h2", "꾸미기 폼"),
+            el("main",
+                preview = new SparrowPreview(metadata.attributes, ment),
+                el(".form",
+                    el(".change-ment",
+                        el("p", "말풍선을 변경하는데는 100 IJM이 필요합니다."),
+                        input = el("input", {
+                            placeholder: "말풍선 멘트",
+                            value: ment,
+                            keyup: () => {
+                                preview.changeMent(input.domElement.value);
                             },
-                            ...p.parts.map((part) => {
-                                // 특수 아이템
-                                if (p.name === "Back" && part.name === "JOKER") {
-                                    return undefined;
-                                }
-                                return el("option", part.name, { value: part.name });
-                            }),
-                        ),
-                    )),
-                    el("button", "변경하기", {
-                        click: async () => {
-                            const owner = await Wallet.loadAddress();
-                            if (owner !== undefined) {
-                                if ((await InjeolmiContract.balanceOf(owner)).lte(await ItemStoreContract.itemPrices(0))) {
-                                    alert("절미 부족");
-                                } else {
-                                    const result = await fetch("https://api.tteok.org/sparrows/checkattributesexists", {
-                                        method: "POST",
-                                        body: JSON.stringify({
-                                            attributes: metadata.attributes,
-                                        }),
-                                    })
-                                    if ((await result.json()).exists === true) {
-                                        alert("똑같이 생긴 참새 존재");
+                        }),
+                        el("button", "변경하기", {
+                            click: async () => {
+                                const owner = await Wallet.loadAddress();
+                                if (owner !== undefined) {
+                                    if ((await InjeolmiContract.balanceOf(owner)).lte(await SparrowsMentorContract.price())) {
+                                        alert("절미 부족");
                                     } else {
-                                        const nonce = await ItemStoreContract.nonces(owner);
-                                        await ItemStoreContract.buyItems([0]);
-                                        await fetch(`https://api.tteok.org/sparrows/${id}/change`, {
+                                        await SparrowsMentorContract.changeMent(id, input.domElement.value);
+                                        setTimeout(async () => {
+                                            await superagent.get(`https://api.tteok.org/sparrows/${id}/refresh`);
+                                            await KlubsLoader.refreshMetadata(SparrowsContract.address, id);
+                                            SkyRouter.refresh();
+                                        }, 2000);
+                                    }
+                                }
+                            },
+                        }),
+                    ),
+                    el(".change-parts",
+                        el("p", "모습을 변경하는데는 100 IJM이 필요합니다."),
+                        ...parts.map((p) => {
+                            let select: DomNode;
+                            const dom = el(".select-wrapper",
+                                el("span", p.name),
+                                select = el("select",
+                                    {
+                                        placeholder: p.name,
+                                        change: (event, select) => {
+                                            const value = (select.domElement as HTMLSelectElement).value;
+                                            const a = metadata.attributes.find((a: any) => a.trait_type === p.name);
+                                            const traits: any = p.parts.filter((part) => part.name === value);
+                                            for (const trait of traits) {
+                                                if (
+                                                    trait.condition === undefined ||
+                                                    metadata.attributes.find((d: any) => d.trait_type === trait.condition.trait && trait.condition.values.includes(d.value) === true) !== undefined
+                                                ) {
+                                                    if (a === undefined) {
+                                                        metadata.attributes.push({ trait_type: p.name, value });
+                                                    } else {
+                                                        a.value = value;
+                                                    }
+                                                    preview.changeAttributes(metadata.attributes);
+                                                    break;
+                                                }
+                                            }
+                                        },
+                                    },
+                                    ...p.parts.map((part) => {
+                                        // 특수 아이템
+                                        if (p.name === "Back" && part.name === "JOKER") {
+                                            return undefined;
+                                        }
+                                        return el("option", part.name, { value: part.name });
+                                    }),
+                                ),
+                            );
+                            const a = metadata.attributes.find((a: any) => a.trait_type === p.name);
+                            if (a !== undefined) {
+                                (select.domElement as HTMLSelectElement).value = a.value;
+                            }
+                            return dom;
+                        }),
+                        el("button", "변경하기", {
+                            click: async () => {
+                                const owner = await Wallet.loadAddress();
+                                if (owner !== undefined) {
+                                    if ((await InjeolmiContract.balanceOf(owner)).lte(await ItemStoreContract.itemPrices(0))) {
+                                        alert("절미 부족");
+                                    } else {
+                                        const result = await fetch("https://api.tteok.org/sparrows/checkattributesexists", {
                                             method: "POST",
                                             body: JSON.stringify({
-                                                address: owner,
-                                                nonce: nonce.toNumber(),
-                                                itemId: 0,
                                                 attributes: metadata.attributes,
                                             }),
-                                        });
-                                        KlubsLoader.refreshMetadata(SparrowsContract.address, id);
-                                        ViewUtil.waitTransactionAndRefresh();
+                                        })
+                                        if ((await result.json()).exists === true) {
+                                            alert("똑같이 생긴 참새 존재");
+                                        } else {
+                                            const nonce = await ItemStoreContract.nonces(owner);
+                                            await ItemStoreContract.buyItems([0]);
+                                            setTimeout(async () => {
+                                                await fetch(`https://api.tteok.org/sparrows/${id}/change`, {
+                                                    method: "POST",
+                                                    body: JSON.stringify({
+                                                        address: owner,
+                                                        nonce: nonce.toNumber(),
+                                                        itemId: 0,
+                                                        attributes: metadata.attributes,
+                                                    }),
+                                                });
+                                                await superagent.get(`https://api.tteok.org/sparrows/${id}/refresh`);
+                                                await KlubsLoader.refreshMetadata(SparrowsContract.address, id);
+                                                SkyRouter.refresh();
+                                            }, 2000);
+                                        }
                                     }
                                 }
-                            }
+                            },
+                        }),
+                    ),
+                ),
+            ),
+            el("h2", "실제 이미지"),
+            el(".real",
+                el("img", { src: metadata.image }),
+                el(".form",
+                    el("p", "실제 이미지가 달라?"),
+                    el("button", "실제 이미지 재생성", {
+                        click: async () => {
+                            await superagent.get(`https://api.tteok.org/sparrows/${id}/refresh`);
+                            SkyRouter.refresh();
                         },
                     }),
                 ),
             ),
-        ));
+        );
     }
 
     public changeParams(params: ViewParams, uri: string): void {
